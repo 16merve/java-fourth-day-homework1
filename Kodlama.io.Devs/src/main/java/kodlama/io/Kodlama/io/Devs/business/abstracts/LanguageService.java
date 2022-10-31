2779cd6016ec8cb6dd64a62242e95677942c4d92
Merve <113795180+16merve@users.noreply.github.com>
2022-10-31 13:50:51 +0300

    Add files via upload

---
+++ Kodlama.io.Devs/src/main/java/kodlama/io/Kodlama/io/Devs/business/abstracts/LanguageService.java
@@ -0,0 +1,19 @@
+package kodlama.io.Kodlama.io.Devs.business.abstracts;
+
+import java.util.List;
+
+import kodlama.io.Kodlama.io.Devs.entities.concretes.Language;
+
+public interface LanguageService {
+     List<Language> getAll();
+
+	void add(Language language);
+
+	void delete(int id);
+
+	void update(int id);
+
+	Object getById(int id);
+
+	List<Language> getById();
+}
